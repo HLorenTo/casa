a3a7ae45d83ce07f536cbc0e1bbe8969e6554e2b
HLorenTo <lorena.torres.h@hotmail.com>
2021-12-13 20:17:33 -0500

    first commit

---
+++ src/casa/Casa.java
@@ -0,0 +1,34 @@
+package casa;
+public class Casa 
+{
+    final private String ciudad = "Facatativa";
+    final private String barrio= "Chicuaza";
+    final private String color="Verde";
+    final private int pisos=2;
+    final private int habitaciones=5;
+    final private int banos=2;
+    final private int cocinas=1;
+
+public String getCiudad (){
+    return ciudad;
+}
+public String getBarrio (){
+    return barrio;
+}
+public String getColor (){
+    return color;
+}
+public int getPisos (){
+    return pisos;
+}
+public int getHabitaciones (){
+    return habitaciones;
+}
+public int getbanos(){
+    return banos;
+}
+public int getCocinas(){
+    return cocinas;
+}
+        
+}
---
+++ src/casa/Principal.java
@@ -0,0 +1,13 @@
+package casa;
+public class Principal {
+public static void main(String[] args) {
+    Casa torres = new Casa(); 
+        System.out.println("Ciudad: "+ torres.getCiudad());
+        System.out.println("Barrio: "+ torres.getBarrio());
+        System.out.println("Color: "+ torres.getColor());
+        System.out.println("Habitaciones: "+torres.getHabitaciones());
+        System.out.println("Baños: "+torres.getbanos());
+        System.out.println("Cocinas: "+ torres.getCocinas());
+        System.out.println("Pisos: "+ torres.getPisos());
+    }    
+}
